a3e082a10b51d4a129673af21565f624e08ea156
Daddy <aimeiradukunda113@gmail.com>
2025-03-18 03:28:24 +1400

    my second commit

--- hi.py
+++ hi.py
@@ -1,16 +1,16 @@
-print("hello")
-n1=input("enter any text you want:")
-print(len(n1))
-
-
-
-
+# print("hello")
 # n1=input("enter any text you want:")
-# count=len(n1)
-# for i in n1:
-#     if i==" ":
-#       count=count-1
-# print(count)
+# print(len(n1))
+
+
+
+
+n1=input("enter any text you want:")
+count=len(n1)
+for i in n1:
+    if i==" ":
+      count=count-1
+print(count)
 
 
 
