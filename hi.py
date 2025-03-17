6f8a04dd21eead92731001cddb2184e97f805758
Daddy <aimeiradukunda113@gmail.com>
2025-03-18 04:44:55 +1400

    my third commit

--- hi.py
+++ hi.py
@@ -5,13 +5,19 @@
 
 
 
-n1=input("enter any text you want:")
-count=len(n1)
-for i in n1:
-    if i==" ":
-      count=count-1
-print(count)
+# n1=input("enter any text you want:")
+# count=len(n1)
+# for i in n1:
+#     if i==" ":
+#       count=count-1
+# print(count)
 
+get=[]
+for i in range(1,6):
+    n1=input("enter the word:\n")
+    get.append(n1)
+for n1 in get:
+    print(len(n1.replace(" ","")))
 
 
 
