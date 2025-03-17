572b276794290baf890f31bb51246aebff740ebb
Daddy <aimeiradukunda113@gmail.com>
2025-03-18 03:19:41 +1400

    my first commit

---
+++ hi.py
@@ -0,0 +1,17 @@
+print("hello")
+n1=input("enter any text you want:")
+print(len(n1))
+
+
+
+
+# n1=input("enter any text you want:")
+# count=len(n1)
+# for i in n1:
+#     if i==" ":
+#       count=count-1
+# print(count)
+
+
+
+
